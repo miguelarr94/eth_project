f14de90aac1cb5216bc6d53f03d378e93032368a
miguelarr94 <miguelarr940927@gmail.com>
2020-11-11 21:26:07 -0800

    Inicializando MyContract

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -1,3 +1,12 @@
-// Smart Contract para el seguimiento de solicitudes de compra.
-
 pragma solidity 0.6.12;
+
+// Smart Contract para el seguimiento de solicitudes de compra.
+contract MyContract {
+
+    // variables
+
+    // constructor
+
+    // metodos
+    
+}
