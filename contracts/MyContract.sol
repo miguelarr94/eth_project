102ce21752b58fb174d1608beab92103fdb43f5b
miguelarr94 <miguelarr940927@gmail.com>
2020-11-11 23:50:31 -0800

    Definiendo variables

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -4,9 +4,23 @@ pragma solidity 0.6.12;
 contract MyContract {
 
     // variables
-
+    address payable public admin;
+    // datos de los usuarios
+    struct DatUsuario {
+        string nombre;
+        string cargo;
+    }
+    // datos de las solicitudes
+    struct DatSolicitud {
+        string id;
+        string descripcion;
+        string estado;
+    }
+    //  Correspondencia identificador (address) del usuario
+    mapping(address => DatUsuario) public usuarios;
+    
     // constructor
 
     // metodos
-    
+
 }
