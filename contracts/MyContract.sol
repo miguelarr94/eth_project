4daab37c5bae07d2a07622106e5fefaf2ebf53fe
miguelarr94 <miguelarr940927@gmail.com>
2020-11-19 12:33:36 -0800

    Inicializacion de variables

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -1,9 +1,11 @@
+// SPDX-License-Identifier: MIT
 pragma solidity 0.6.12;
 
 // Smart Contract para el seguimiento de solicitudes de compra.
 contract MyContract {
 
-    // variables
+// variables
+
     address payable public sysadmin;
     // datos de los usuarios
     struct DatUsuario {
@@ -26,15 +28,28 @@ contract MyContract {
         string nota;
         string fecha;
         bool correccion;
-        string n_usuario;
+        string nom_usuario;
     }
     // posibles estados de las solicitudes
     string[] public estados;
+    // arreglo de usuarios
+    DatUsuario[] usuarios;
+    // arreglo de usuarios
+    DatSolicitud[] solicitudes;
     //  Correspondencia identificador (address) del usuario
     mapping(address => DatUsuario) public Usuarios;
     
-    // constructor
+// constructor
+    constructor() public {
+        sysadmin = msg.sender;
+        estados = ["Solicitud generada", "Solicitud autorizada", "Solicitud cotizada", "Recibo de cotizaciones de proveedores", "Adjudicada a proveedor", "Orden de compra generada", "Recibo del producto y factura", "Pago efectuado", "Orden de compra finiquitada"];
+        usuarios.push(DatUsuario('Juan', 'SysAdmin'));
+        usuarios.push(DatUsuario('Pedro', 'Administrador de FCAyS'));
+    }
 
-    // metodos
+// metodos
+
+    //registrar la direccion Ethereum (id) de los usuarios
+
 
 }
