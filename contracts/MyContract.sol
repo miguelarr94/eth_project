00ca31554d14d3c2277b47ac92dc3ea61dbb982e
miguelarr94 <miguelarr940927@gmail.com>
2020-11-19 16:21:43 -0800

    metodo comprobacion de usuarios

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -32,19 +32,22 @@ contract MyContract {
     }
     // posibles estados de las solicitudes
     string[] public estados;
-    // arreglo de usuarios
-    // DatUsuario[] usuarios;
+    // arreglo de id de usuarios
+    address[] ids;
     // arreglo de solicitudes
-    DatSolicitud[] solicitudes;
+    //DatSolicitud[] solicitudes;
     //  Correspondencia identificador (address) del usuario
     mapping(address => DatUsuario) public Usuarios;
+
+// eventos
+    // usuario registrado
+    event RegUsuario(address indexed idusuario, string nombre, string cargo);
+
     
 // constructor
     constructor() public {
         sysadmin = msg.sender;
         estados = ["Solicitud generada", "Solicitud autorizada", "Solicitud cotizada", "Recibo de cotizaciones de proveedores", "Adjudicada a proveedor", "Orden de compra generada", "Recibo del producto y factura", "Pago efectuado", "Orden de compra finiquitada"];
-        //usuarios.push(DatUsuario('Juan', 'SysAdmin'));
-        //usuarios.push(DatUsuario('Pedro', 'Administrador de FCAyS'));
     }
 
 // metodos
@@ -53,6 +56,9 @@ contract MyContract {
     function regId(address idusuario, string memory nombre, string memory cargo) public {
         require(msg.sender == sysadmin);
         Usuarios[idusuario] = DatUsuario(nombre, cargo); // Agregar usuario al mapping Usuarios
+        ids.push(idusuario);
+
+        emit RegUsuario(idusuario, nombre, cargo); // evento de registro de usuario
     }
 
     // ver susarios registrados
@@ -64,4 +70,23 @@ contract MyContract {
         return string(abi.encodePacked("Nombre: ",nombre," Cargo: ",cargo));
     }
 
+    // comprobar usuario registrado
+    function compUsuario(address idusuario) internal view returns(bool) {
+        bool band;
+        for(uint i = 0; i < ids.length; i++) {
+            if(idusuario == ids[i]) {
+                band = true;
+                break;
+            }
+        }
+        return band;
+    }
+
+    // dar de alta solicitud de compra
+    function nuevaSolicitud() public view returns(string memory) {
+        require(compUsuario(msg.sender) == true);
+        return "Comprobacion correcta";
+    }
+
+    // cambiar estado e informacion de solicitud de compra
 }
